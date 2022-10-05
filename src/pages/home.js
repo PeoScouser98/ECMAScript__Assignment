916b4e39f5e969da3a758a24b78a554f2773d93a
quanghiep031 <quanghiep03198@gmail.com>
2022-10-06 00:24:28 +0700

    home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,66 +3,50 @@ import Header from "../components/header";
 import Sidebar from "../components/sidebar";
 import HeroBanner from "../components/hero";
 import Footer from "../components/footer";
+import ProductCard from "../components/product-card_v2";
 
 const Homepage = {
 	async render() {
 		const products = await instance.get("/products/?_expand=category");
 		const categories = await instance.get("/categories");
-		console.log(categories);
 		return /* html */ `
         <div class="max-w-6xl mx-auto  drawer">
             <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
-				<div class="drawer-content invisible-scroll flex flex-col">
+            <div class="drawer-content invisible-scroll flex flex-col">
                 ${Header.render()}
 
 				  <main class="py-10 px-5 flex flex-col gap-12">
-              <!-- hero banner-->
-				  <section>${HeroBanner.render()}</section>
-				  
-              <!-- products list-->
-					<section>
-					<h1 class="text-center text-5xl font-bold mb-6">Our Popular Dishes</h1>
-					<p class="text-center text-base mb-6 max-w-[700px] mx-auto">Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </p>
-					<div class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-x-10 gap-y-14">
-                    ${products
-						.map((item) => {
-							return /* html */ `
-						<div class="flex flex-col gap-5">
-						<a href="/#/products/${item.id}"><img src="${item.image}" class="rounded-2xl"/></a>
-						<h3 class="text-[#F54748] font-semibold text-lg">${item.name}</h3>
-						<div class="flex items-start gap-10">
-							<span class="inline-flex items-center gap-1"><img src="../../assets/img/Timer.svg" alt="" loading="lazy"> 30s</span>
-							<span class="inline-flex item-center gap-1">
-								<img src="../../assets/img/ForkKnife.svg" alt="" loading="lazy"> ${item.category.name}
-							</span>
-						</div>
-                    </div>
-                  `;
-						})
-						.join("")}
-                  </div>
-                </section>
-				  
-					<!-- categories list -->
-                <section>
-                  <div class="flex justify-between items-center mb-20">
-                    <h1 class="text-5xl font-bold">Category</h1>
-                    <a type="button" role="button" href="#categories" class="border border-[#F54748] text-[#F54748] py-[19px] px-[24px] rounded-xl">View All Categories</a>
-                  </div>
+						<!-- hero banner-->
+						<section>${HeroBanner.render()}</section>
             
-                  <div class="flex justify-between items-center overflow-x-auto invisible-scroll" id="categories">
-                    ${categories
-						.map((item) => {
-							return /* html */ `
-                    <div class="flex flex-col gap-5 items-center">
-                      <a href=""><img src="${item.image}"/></a>
-                      <span>${item.name}</span>
-                    </div>
-                  `;
-						})
-						.join("")}
-                  </div>
-                </section>
+						<!-- products list-->
+						<section>
+							<h1 class="text-center text-5xl font-bold mb-6">Our Popular Dishes</h1>
+							<p class="text-center text-base mb-6 max-w-[700px] mx-auto">Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </p>
+							<div class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-x-10 gap-y-14">
+								${products.map((item) => ProductCard.render(item)).join("")}
+							</div>
+						</section>
+				  
+							<!-- categories list -->
+						<section>
+							<div class="flex justify-between items-center mb-20">
+								<h1 class="text-5xl font-bold">Category</h1>
+								<a type="button" role="button" href="#categories" class="border border-[#F54748] text-[#F54748] py-[19px] px-[24px] rounded-xl">View All Categories</a>
+							</div>
+					
+							<div class="flex justify-between items-center overflow-x-auto invisible-scroll" id="categories">
+									${categories
+										.map(
+											(item) => /* html */ `
+									<div class="flex flex-col gap-5 items-center">
+										<a href=""><img src="${item.image}"/></a>
+										<span>${item.name}</span>
+									</div>`,
+										)
+										.join("")}
+							</div>
+						</section>
 					 </main>
 					 <hr>
 					 <footer> ${Footer.render()}</footer>
