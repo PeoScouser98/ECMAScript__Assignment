769acc773edc713c1e70e0bf9397ba48199d591a
quanghiep031 <quanghiep03198@gmail.com>
2022-10-16 18:59:54 +0700

    feat: create product

--- src/pages/admin/product-add.js
+++ src/pages/admin/product-add.js
@@ -1,4 +1,3 @@
-import * as filestack from "filestack-js";
 import Header from "../../components/admin.header";
 import Footer from "../../components/footer";
 import Sidebar from "../../components/admin.sidebar";
@@ -8,96 +7,93 @@ import validation from "../../utils/validate";
 import { create } from "../../api/product";
 import toast from "../../components/toast";
 
+import firebaseUpload from "../../services/firebase";
+
 const AddProductPage = {
 	async render() {
 		const categories = await instance.get("/categories");
-		return /* html */ `
-         <div class="drawer">
-            <input type="checkbox" name="" class="drawer-toggle" id="my-drawer-3">
-            <div class="drawer-content ">
-                 <header class="max-w-6xl mx-auto">
-               ${Header.render()}
-            </header>
-            <main class="my-10 bg-zinc-100 p-10">
-            <form action="" id="add-product--form" class="max-w-3xl mx-auto bg-white rounded-2xl shadow-2xl p-5">
-               <h1 class="text-center font-semibold text-3xl">Add Product</h1>
-                  <div class="form-control gap-1 mb-5">
-                     <label for="">Product's name</label>
-                     <input type="text" id="name" class="input input-bordered" data-name="">
-                     <small class="text-error error-message"></small>
-                  </div>
-                  <div class="form-control gap-1 mb-5">
-                     <label for="">Price</label>
-                     <input type="number" id="price" class="input input-bordered" data-name="">
-                     <small class="text-error error-message"></small>
-                  </div>
-                  <div class="form-control gap-1 mb-5">
-                     <label for="">Stock</label>
-                     <input type="number" min="0" id="stock" class="input input-bordered" data-name="">
-                     <small class="text-error error-message"></small>
-                  </div>
-              
-                  <div class="form-control gap-1 mb-5">
-                     <label for=""></label>
-                     <select name="" id="cate" class="select select-bordered" data-name="">
-                        ${categories.map((cate) => /* html */ `<option value="${cate.id}">${cate.name}</option>`)}
-                     </select>
-                     <small class="text-error error-message"></small>
-                  </div>
-                  <div class="form-control gap-1 mb-5">
-                     <label for="">Image</label>
-                     <button type="button" id="upload-btn" class="btn btn-primary w-fit">Choose image</button>
-                     <small class="text-error error-message"></small>
-                  </div>
-                   <div class="form-control gap-1 mb-5">
-                     <label for="">Description</label>
-                     <textarea name="" id="desc" cols="30" rows="10" class="textarea textarea-bordered" data-name=""></textarea>
-                     <small class="text-error error-message"></small>
-                  </div> 
-                  <button type="submit" class="btn btn-primary btn-block">Create new</button>
-               </form>
-            </main>
-            <footer class="max-w-6xl mx-auto">
-               ${Footer.render()}
-            </footer>
-            </div>
-          ${Sidebar.render()}
-         </div>
+
+		return /* html */`
+			<div class="drawer drawer-mobile">
+				<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+				<div class="drawer-content flex flex-col justify-start gap-10 items-stretch max-h-screen bg-">
+					<header class="min-w-full"> ${await Header.render()}</header>
+
+					<div class="flex-1 px-10 sm:px-5">
+						<form action="" id="edit-product--form" class="max-w-full mx-auto bg-white rounded-2xl shadow-2xl sm:p-5 p-10">
+							<h1 class="text-center font-semibold text-3xl mb-10">Add product</h1>
+							<div class="grid grid-cols-2 gap-10 sm:grid-cols-1 sm:gap-5 mb-10">
+								<div class="flex flex-col justify-between sm:justify-start" >
+									<div class="form-control gap-1 mb-5">
+										<label class="font-medium italic" for="">Product's name</label>
+										<input type="text" id="name" class="input input-bordered" data-name="Product's name">
+										<small class="text-error error-message"></small>
+									</div>
+									<div class="form-control gap-1 mb-5">
+										<label class="font-medium italic" for="">Price</label>
+										<input type="number" id="price" class="input input-bordered" data-name="Price">
+										<small class="text-error error-message"></small>
+									</div>
+									<div class="form-control gap-1 mb-5">
+										<label class="font-medium italic" for="">Stock</label>
+										<input type="number" min="0" id="stock" class="input input-bordered" data-name="Stock">
+										<small class="text-error error-message"></small>
+									</div>
+		
+									<div class="form-control gap-1 mb-5">
+										<label class="font-medium italic" for="">Category</label>
+										<select name="" id="cate" class="select select-bordered" data-name="Category">
+											${categories.map((cate) =>/* html */ `<option value="${cate.id}">${cate.name}</option>`,)}
+											</select>
+										<small class="text-error error-message"></small>
+									</div>
+
+									
+								</div>
+
+								<div>
+									<div class="form-control gap-1 mb-5">
+										<label class="font-medium italic" for="">Image</label>
+										<input type="file" name="" id="file" class="file hidden" data-name="Image"  accept="image/*">
+										<div class="relative flex justify-center items-center w-[200px] h-[200px] border rounded-lg overflow-hidden">
+											<label class="font-medium italic relative z-10 h-full w-full flex justify-center items-center bg-transparent text-zinc-300 duration-500 hover:bg-zinc-900 hover:text-white" for="file" >Upload</label>
+											<img src="" alt="" id="preview" class="absolute top-0 left-0 max-w-full h-full  object-cover object-center z-0">
+										</div>
+										<small class="text-error error-message"></small>
+									</div> 
+									<div class="form-control gap-1 mb-5">
+										<label class="font-medium italic" for="">Description</label>
+										<textarea name="" id="desc" cols="20" rows="5" class="textarea textarea-bordered" data-name="Description"></textarea>
+										<small class="text-error error-message"></small>
+									</div> 
+									
+								</div>
+							</div>
+								
+								<button type="submit" class="btn btn-primary w-fit">Save</button>
+							</form>
+						</div>
+					</div>
+					${await Sidebar.render()}
+				</div>
       `;
 	},
 	handleEvents() {
-		const { role } = JSON.parse(localStorage.getItem("auth"));
-		if (role != 1) {
-			toast("error", "You don't have permission to access to this page!");
-			window.location.href = "/#/";
-		}
-
 		Header.handleEvent();
 
-		let file;
-		const uploadBtn = $("#upload-btn");
-		if (uploadBtn)
-			uploadBtn.addEventListener("click", () => {
-				const client = filestack.init("AfLOI3jGURSCQm8UcHQD3z");
-				const option = {
-					accept: [".png", ".jpg", ".jpeg", ".webp"],
-					acceptFn: (file, options) => {
-						const mimeFromExtension = options.mimeFromExtension(file.originalFile.name);
-						if (options.acceptMime.length && !options.acceptMime.includes(mimeFromExtension)) {
-							return Promise.reject("Cannot accept that file");
-						}
-						return Promise.resolve();
-					},
-					onUploadDone: ({ filesUploaded }) => {
-						file = filesUploaded[0].url;
-						console.log(file);
-					},
-				};
-				client.picker(option).open();
-			});
+		// preview before uploading
+		const file = $("#file")
+		if (file)
+			file.onchange = (e) => {
+				const url = URL.createObjectURL(e.target.files[0])
+				const preview = $("#preview")
+				if (preview)
+					preview.src = url
+			}
+
 		const addProductForm = $("#add-product--form");
 		if (addProductForm) {
-			addProductForm.addEventListener("submit", (event) => {
+			addProductForm.addEventListener("submit", async (event) => {
 				event.preventDefault();
 				const productName = $("#name");
 				const price = $("#price");
@@ -105,20 +101,32 @@ const AddProductPage = {
 				const category = $("#cate");
 				const desc = $("#desc");
 
-				if (!validation.areRequired(productName, price, stock, category, desc)) return;
+				// if not pass through validation -> stop submitting
+				if (!validation.areRequired(productName, price, stock, category, desc, file)) return;
 
-				const newProduct = {
-					name: productName.value,
-					price: price.value,
-					stock: stock.value,
-					categoryId: category.value,
-					desc: desc.value,
-					image: file,
-				};
-				console.log(newProduct);
-				const res = create(newProduct);
-				if (res) toast("success", "Create product successfully!");
-				event.target.reset();
+				try {
+					const image = await firebaseUpload(file.files[0])
+					console.log(image && typeof image == "string");
+					if (image) {
+						const newProduct = {
+							name: productName.value,
+							price: price.value,
+							stock: stock.value,
+							categoryId: category.value,
+							desc: desc.value,
+							image: image,
+						};
+
+						const res = await create(newProduct);
+						if (res) toast("success", "Create product successfully!");
+
+						console.log(res);
+						event.target.reset();
+					}
+				} catch (error) {
+					return Promise.reject(error)
+				}
+
 			});
 		}
 	},
