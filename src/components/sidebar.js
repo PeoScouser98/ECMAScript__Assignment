b1096a114233c2030549a6fe100af18324cf920e
quanghiep031 <quanghiep03198@gmail.com>
2022-10-16 18:58:33 +0700

    update component

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,17 +1,17 @@
 const Sidebar = {
-	render() {
-		return /* html */ `
+  render() {
+    return /* html */ `
        <div class="drawer-side">
       <label for="my-drawer-3" class="drawer-overlay"></label> 
       <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
         <!-- Sidebar content here -->
-        <li><a>Home</a></li>
-        <li><a>Product</a></li> 
-        <li><a>About Us</a></li>
-        <li><a>Contact Us</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/#/products">Product</a></li> 
+        <li><a href="/#/about">About Us</a></li>
+        <li><a href="/#/contact">Contact Us</a></li>
       </ul>
     </div>
           `;
-	},
+  },
 };
 export default Sidebar;
