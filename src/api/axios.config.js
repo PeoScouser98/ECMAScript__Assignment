110fa6644043e54c9ac2ce0e4f0b8e6993113929
quanghiep031 <quanghiep03198@gmail.com>
2022-10-16 18:59:06 +0700

    axios config

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -1,10 +1,15 @@
 import axios from "axios";
+import { refreshToken } from "./auth";
 
 const instance = axios.create({
-	baseURL: "http://my-json-server.typicode.com/PeoScouser98/ecmascript/",
+	baseURL: "http://localhost:3001",
 });
 instance.interceptors.request.use(
 	(config) => {
+		const accessToken = instance.getAccessToken()
+		if (accessToken)
+			config.headers.authorization = `Bearer ${accessToken}`
+
 		return config;
 	},
 	(error) => {
@@ -13,12 +18,30 @@ instance.interceptors.request.use(
 );
 
 instance.interceptors.response.use(
-	(response) => {
+	async (response) => {
+
+		if (response.data.statusCode == 401) {
+			console.log("Access token has been expired!")
+			const { accessToken } = await refreshToken()
+			instance.setAccessToken(accessToken)
+			console.log("New access token: ", accessToken);
+		}
+
+
+
 		return response.data;
 	},
 	(error) => {
 		return Promise.reject(error);
 	},
 );
+
+instance.getAccessToken = () => {
+	return localStorage.getItem("accessToken")
+}
+
+instance.setAccessToken = (accessToken) => {
+	localStorage.setItem("accessToken", accessToken)
+}
 
 export default instance;
